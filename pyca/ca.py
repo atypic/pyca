76b3191c957a541ed50a723f660cdee460338a63
Odd Rune <oddrunesl@gmail.com>
2021-07-19 18:55:23 +0200

    meh

--- pyca/ca.py
+++ pyca/ca.py
@@ -1,29 +1,44 @@
 import numpy as np
 
 class ElementaryCA():
-    def __init__(self, size = 32, ruleset = 110, max_timesteps = 1000, startstate = 'random'):
+    def __init__(self, size = 32, ruleset = 110, save_timesteps = 1000, startstate = 'random'):
         self.size = 32
-        self.cell_grid = np.zeros((max_timesteps + 1, self.size), dtype=int)
+        self.history = np.zeros((save_timesteps, self.size), dtype=int)
+        #self.current_state = np.zeros((save_timesteps + 1, self.size), dtype=int)
         if startstate == 'random': 
-            self.cell_grid[0] =  np.random.randint(2, size=self.size)
+            self.current_state =  np.random.randint(2, size=self.size)
         else:
-            self.cell_grid[0] = startstate
+            self.current_state = startstate
+        self.history[0] = self.current_state
 
         self.ruleset = 110
-        self.max_timesteps = 1000
+        self.max_save_timesteps = save_timesteps
+        self.steps = 0
      
     def step_ca(self):#, timesteps, width, ruleset, startstate):
-        g = self.cell_grid
+        g = self.current_state
         W = self.size
-        for j in range(self.max_timesteps):
+        while True:
             for i in range(self.size):
-                setrule = (g[j, (i - 1) % W]) << 2 | \
-                          (g[j, i % W]) << 1 | \
-                          (g[j, (i + 1) % W])
+                setrule = (g[(i - 1) % W]) << 2 | \
+                          (g[i % W]) << 1 | \
+                          (g[(i + 1) % W])
                 next_cell_state = (self.ruleset >> setrule) & 1
-                self.cell_grid[j + 1, i] = next_cell_state
+                self.current_state[i] = next_cell_state
+            
+            self.history[self.steps % self.max_save_timesteps] = self.current_state
+            self.steps += 1
 
-            yield self.cell_grid[j + 1]
+            yield self.current_state
+    
+    def get_history(self):
+        #build a continous history.
+        overflow = self.steps % self.max_save_timesteps
+        print(overflow)
+        if overflow > 0:
+            return np.concatenate((self.history[overflow:], self.history[:overflow]))
+        else:
+            return self.history[:self.steps]
 
     @staticmethod
     def bool2int(x):
@@ -44,11 +59,9 @@ if __name__ == "__main__":
     xint = []
     yint = []
     ca_rule = 142
-    for time, state in enumerate(step_ca(t, w, ca_rule, initial_state)):
-        x = state[0:int(w/2)]
-        y = state[int(w/2):]
+    ca = ElementaryCA(size=w, ruleset=ca_rule, save_timesteps=10, startstate='random')
+    for time, state in enumerate(ca.step_ca()):
+        if time > 1000:
+            break
 
-        intx = bool2int(x)
-        inty = bool2int(y)
-        xint.append(intx)
-        yint.append(inty)
+    print(ca.get_history())
